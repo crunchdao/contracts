fb0d075c181fcf635578b915613788d176a6420c
Caceresenzo <caceresenzo1502@gmail.com>
2021-08-31 23:36:24 +0200

    feat(contract/distribute): reworked

--- contracts/CrunchDistribute.sol
+++ contracts/CrunchDistribute.sol
@@ -1,22 +1,18 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
 
-pragma solidity ^0.8.0;
+import "./HasCrunchParent.sol";
+import "./CrunchToken.sol";
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract CrunchDistribute is HasCrunchParent {
+    constructor(CrunchToken _crunch) HasCrunchParent(_crunch) {}
 
-contract distributeTokens {
-    
-    constructor() { owner = payable(msg.sender); }
-    address payable owner;
-       
-    function distribute(address[] memory recipients, uint256[] memory values) public payable {
-        
-        require(msg.sender == owner, "Only owner can call this function.");
-
-        address sender = address(0xaE783Bcc9615c1736e1DEAd4DCB098Dd43434a10);
-        
+    function distribute(address[] memory recipients, uint256[] memory values)
+        public
+        onlyOwner
+    {
         for (uint256 i = 0; i < recipients.length; i++) {
-            sender.transfer(recipients[i], values[i]);
+            crunch.transfer(recipients[i], values[i]);
         }
     }
 }
