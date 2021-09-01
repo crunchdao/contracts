de6037137df9f1917adbff682044308a1d029519
Caceresenzo <caceresenzo1502@gmail.com>
2021-09-01 15:12:09 +0200

    feat(contract/airdrop): added condition to ensure reserve has enough token for the distribution

--- contracts/CrunchAirdrop.sol
+++ contracts/CrunchAirdrop.sol
@@ -7,6 +7,14 @@ import "./CrunchToken.sol";
 contract CrunchAirdrop is HasCrunchParent {
     constructor(CrunchToken _crunch) HasCrunchParent(_crunch) {}
 
+    /**
+     * @dev Distribute tokens.
+     *
+     * Requirements:
+     *
+     * - `recipients` and `values` are the same length.
+     * - the reserve has enough to cover the sum of `values`.
+     */
     function distribute(address[] memory recipients, uint256[] memory values)
         public
         onlyOwner
@@ -16,12 +24,29 @@ contract CrunchAirdrop is HasCrunchParent {
             "recipients and values length differ"
         );
 
+        require(
+            reserve() >= sum(values),
+            "the reserve does not have enough token"
+        );
+
         for (uint256 index = 0; index < recipients.length; index++) {
             crunch.transfer(recipients[index], values[index]);
         }
     }
 
+    /** @dev Returns the current balance of the contract. */
     function reserve() public view returns (uint256) {
         return crunch.balanceOf(address(this));
+    }
+
+    /** @dev Returns the sum of each value in `values`. */
+    function sum(uint256[] memory values)
+        internal
+        pure
+        returns (uint256 accumulator)
+    {
+        for (uint256 index = 0; index < values.length; index++) {
+            accumulator += values[index];
+        }
     }
 }
