3119369084cea95831ad041ef66e5cee3ef3eec1
Caceresenzo <caceresenzo1502@gmail.com>
2021-09-01 16:23:32 +0200

    feat(contract/vesting): added basic from openzeppelin

---
+++ contracts/CrunchVesting.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "./TokenVesting.sol";
+
+contract CrunchVesting is TokenVesting {
+    constructor(address beneficiary, uint256 cliffDurationInYear, uint256 durationInYear) public
+        TokenVesting(beneficiary, block.timestamp, cliffDuration * 365.25 days, durationInYear * 365.25 days, true) {
+    }
+}
