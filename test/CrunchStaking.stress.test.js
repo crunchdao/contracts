d774a29c01a52bb9dc18f6c7bf34b5e00954319c
Caceresenzo <caceresenzo1502@gmail.com>
2021-09-08 22:28:44 +0200

    test(contract/staking): stressing `setRewardPerDay(uint256)`

--- test/CrunchStaking.stress.test.js
+++ test/CrunchStaking.stress.test.js
@@ -8,6 +8,8 @@ const { shuffle } = require("./helper/shuffle");
 const CrunchToken = artifacts.require("CrunchToken");
 const CrunchStaking = artifacts.require("CrunchStaking");
 
+const REWARD_PER_DAY_1_PERCENT_PER_MONTH = 325.5;
+
 contract("Crunch Staking [ Stress ]", async (accounts) => {
   let crunch;
   let staking;
@@ -41,11 +43,15 @@ contract("Crunch Staking [ Stress ]", async (accounts) => {
   const withdraw = async (from) =>
     await expect(staking.withdraw({ from })).to.be.fulfilled;
 
-  const nextMonth = async () => await advance.timeAndBlock(time.days(31));
+  const nextMonth = async (month = 1) =>
+    await advance.timeAndBlock(time.days(31 * month));
 
   beforeEach(async () => {
     crunch = await CrunchToken.new();
-    staking = await CrunchStaking.new(crunch.address, 657);
+    staking = await CrunchStaking.new(
+      crunch.address,
+      REWARD_PER_DAY_1_PERCENT_PER_MONTH * 2
+    );
 
     for (const staker of stakers) {
       await crunch.transfer(staker, initialStakerBalance);
@@ -55,56 +61,99 @@ contract("Crunch Staking [ Stress ]", async (accounts) => {
   it("deposit & withdraw", async () => {
     const amount = 100;
 
-    {
-      await crunch.transfer(staking.address, 10_000);
+    await crunch.transfer(staking.address, 10_000);
 
-      for (const staker of stakers) {
-        await deposit(staker, amount);
-      }
+    for (const staker of stakers) {
+      await deposit(staker, amount);
+    }
 
-      await isStaking(stakers);
+    await isStaking(stakers);
 
-      const unordered = shuffle(stakers);
-      for (let month = 0; month < unordered.length; month++) {
-        const staker = unordered[month];
+    const unordered = shuffle(stakers);
+    for (let month = 0; month < unordered.length; month++) {
+      const staker = unordered[month];
 
-        await rewardOf(staker, Math.floor(month * amount * 0.02));
+      await rewardOf(staker, Math.floor(month * amount * 0.02));
 
-        await isStaking(unordered.slice(month, unordered.length));
+      await isStaking(unordered.slice(month, unordered.length));
 
-        await withdraw(staker);
+      await withdraw(staker);
 
-        await isNotStaking(unordered.slice(0, month));
+      await isNotStaking(unordered.slice(0, month));
 
-        await nextMonth();
-      }
+      await nextMonth();
     }
   });
 
-  it("deposit & withdraw", async () => {
+  it("deposit & withdraw : no reserve for rewards", async () => {
     const amount = 100;
 
-    {
-      for (const staker of stakers) {
-        await deposit(staker, amount);
+    for (const staker of stakers) {
+      await deposit(staker, amount);
+    }
+
+    await isStaking(stakers);
+
+    const unordered = shuffle(stakers);
+    for (let month = 0; month < unordered.length; month++) {
+      const staker = unordered[month];
+
+      await rewardOf(staker, Math.floor(month * amount * 0.02));
+
+      await isStaking([staker]);
+
+      await expect(staking.withdraw({ from })).to.be.rejected;
+
+      await isStaking([staker]);
+
+      await nextMonth();
+    }
+  });
+
+  it("setRewardPerDay(uint256)", async () => {
+    const amount = 100;
+
+    for (const staker of stakers) {
+      await deposit(staker, amount);
+
+      await nextMonth();
+    }
+
+    for (let index = 0; index < stakers.length; index++) {
+      const staker = stakers[index];
+
+      const at2Percent = Math.floor((stakers.length - index) * amount * 0.02);
+
+      await rewardOf(staker, at2Percent);
+    }
+
+    const tests = [0.06, 0.1, 0.15, 0.1, 0.04];
+
+    const wait = 4; /* month */
+
+    for (let testIndex = 0; testIndex < tests.length; testIndex++) {
+      await expect(
+        staking.setRewardPerDay(
+          Math.floor(
+            REWARD_PER_DAY_1_PERCENT_PER_MONTH * (tests[testIndex] * 100)
+          )
+        )
+      ).to.be.fulfilled;
+
+      for (let index = 0; index < stakers.length; index++) {
+        const staker = stakers[index];
+
+        const at2Percent = Math.floor((stakers.length - index) * amount * 0.02);
+
+        let sum = 0;
+        for (let testSumIndex = 0; testSumIndex < testIndex; testSumIndex++) {
+          sum += Math.floor(wait * amount * tests[testSumIndex]);
+        }
+
+        await rewardOf(staker, at2Percent + sum);
       }
 
-      await isStaking(stakers);
-
-      const unordered = shuffle(stakers);
-      for (let month = 0; month < unordered.length; month++) {
-        const staker = unordered[month];
-
-        await rewardOf(staker, Math.floor(month * amount * 0.02));
-
-        await isStaking([staker]);
-
-        await expect(staking.withdraw({ from })).to.be.rejected;
-
-        await isStaking([staker]);
-
-        await nextMonth();
-      }
+      await nextMonth(wait);
     }
   });
 });
