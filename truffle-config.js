0cd19748c72d9c6318eac8be15c0582b15cd3f24
Caceresenzo <caceresenzo1502@gmail.com>
2021-10-19 17:13:23 +0200

    chore(truffle): forcing 0.8.7 compiler version

--- truffle-config.js
+++ truffle-config.js
@@ -132,7 +132,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "^0.8.0",
+      version: "0.8.7",
 
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
