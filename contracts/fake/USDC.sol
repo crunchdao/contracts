3dd1a739940ac46becf47daa53528439b8d580c1
Caceresenzo <caceresenzo1502@gmail.com>
2022-01-03 18:08:06 +0100

    feat(contract): added fake usdc

---
+++ contracts/fake/USDC.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract USDCoin is ERC20, Ownable {
+    constructor() ERC20("USD Coin", "USDC") {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
