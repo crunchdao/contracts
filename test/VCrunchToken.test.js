b759560b3b2e6069b7761d776e374cca94ef9257
Caceresenzo <caceresenzo1502@gmail.com>
2022-02-17 23:27:55 +0100

    test(contract/v-token): `addBalanceOfs(address, address)`

--- test/VCrunchToken.test.js
+++ test/VCrunchToken.test.js
@@ -5,6 +5,7 @@ const time = require("./helper/time");
 const CrunchToken = artifacts.require("CrunchToken");
 const VCrunchToken = artifacts.require("VCrunchToken");
 const CrunchMultiVesting = artifacts.require("CrunchMultiVesting");
+const CrunchVesting = artifacts.require("CrunchVesting");
 const CrunchVestingV2 = artifacts.require("CrunchVestingV2");
 
 const NULL = "0x0000000000000000000000000000000000000000";
@@ -17,6 +18,10 @@ contract("vCRUNCH Token", async (accounts) => {
   let vCrunch;
 
   const [owner, beneficiary, user] = accounts;
+
+  const fromBeneficiary = {
+    from: beneficiary,
+  };
 
   const fromUser = {
     from: user,
@@ -33,6 +38,61 @@ contract("vCRUNCH Token", async (accounts) => {
 
   it("crunch()", async () => {
     await expect(vCrunch.crunch()).to.eventually.be.equal(crunch.address);
+  });
+
+  it("addBalanceOf(address, address) : vesting", async () => {
+    const amount = new BN(100);
+    const cliff = time.days(1);
+    const duration = time.days(10);
+
+    const vesting = await CrunchVesting.new(
+      crunch.address,
+      NULL /* override owner */,
+      beneficiary,
+      cliff,
+      duration,
+      false /* revokable */
+    );
+
+    const target = vesting.address;
+    const index = ZERO;
+
+    await await expect(
+      vCrunch.addBalanceOf(beneficiary, target, fromUser)
+    ).to.be.rejectedWith(Error, "Ownable: caller is not the owner");
+
+    await await expect(vCrunch.addBalanceOf(beneficiary, target)).to.be
+      .fulfilled;
+
+    await expect(vCrunch.balanceOfs(beneficiary, index)).to.eventually.be.equal(
+      target
+    );
+
+    await expect(
+      vCrunch.balanceOf(beneficiary)
+    ).to.eventually.be.a.bignumber.equal(ZERO);
+
+    await expect(crunch.transfer(vesting.address, amount)).to.be.fulfilled;
+
+    await expect(
+      vCrunch.balanceOf(beneficiary)
+    ).to.eventually.be.a.bignumber.equal(amount);
+
+    await advance.timeAndBlock(time.days(5));
+
+    await expect(vesting.release(fromBeneficiary)).to.be.fulfilled;
+
+    await expect(
+      vCrunch.balanceOf(beneficiary)
+    ).to.eventually.be.a.bignumber.equal(amount.divn(2));
+
+    await advance.timeAndBlock(time.days(5));
+
+    await expect(vesting.release(fromBeneficiary)).to.be.fulfilled;
+
+    await expect(
+      vCrunch.balanceOf(beneficiary)
+    ).to.eventually.be.a.bignumber.equal(ZERO);
   });
 
   it("addMultiple(address, string calldata) : multi-vesting", async () => {
