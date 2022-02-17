759e80277f954005ad7cfa70877c80f0cb6c4187
Caceresenzo <caceresenzo1502@gmail.com>
2022-02-17 23:00:53 +0100

    test(contract/v-token): added

---
+++ test/VCrunchToken.test.js
@@ -0,0 +1,90 @@
+const advance = require("./helper/advance");
+const { expect, BN } = require("./helper/chai");
+const time = require("./helper/time");
+
+const CrunchToken = artifacts.require("CrunchToken");
+const VCrunchToken = artifacts.require("VCrunchToken");
+const CrunchMultiVesting = artifacts.require("CrunchMultiVesting");
+const CrunchVestingV2 = artifacts.require("CrunchVestingV2");
+
+const NULL = "0x0000000000000000000000000000000000000000";
+const ZERO = new BN("0");
+const ONE = new BN("1");
+const TWO = new BN("2");
+
+contract("vCRUNCH Token", async (accounts) => {
+  let crunch;
+  let vCrunch;
+
+  const [owner, beneficiary, user] = accounts;
+
+  const fromUser = {
+    from: user,
+  };
+
+  beforeEach(async () => {
+    crunch = await CrunchToken.new();
+    vCrunch = await VCrunchToken.new(crunch.address);
+  });
+
+  it("owner()", async () => {
+    await expect(vCrunch.owner()).to.eventually.be.equal(owner);
+  });
+
+  it("crunch()", async () => {
+    await expect(vCrunch.crunch()).to.eventually.be.equal(crunch.address);
+  });
+
+  it("addMultiple(address, string calldata) : multi-vesting", async () => {
+    const amount = new BN(100);
+
+    const multiVesting = await CrunchMultiVesting.new(crunch.address);
+    await expect(crunch.transfer(multiVesting.address, amount)).to.be.fulfilled;
+
+    const target = multiVesting.address;
+    const signature = "balanceOf(address)";
+    const index = ZERO;
+
+    await await expect(
+      vCrunch.addMultiple(target, signature, fromUser)
+    ).to.be.rejectedWith(Error, "Ownable: caller is not the owner");
+
+    await expect(vCrunch.addMultiple(target, signature)).to.be.fulfilled;
+
+    await expect(vCrunch.multiples(index))
+      .to.eventually.have.property("target")
+      .and.equal(target);
+
+    await expect(vCrunch.multiples(index))
+      .to.eventually.have.property("signature")
+      .and.equal(signature);
+
+    await expect(
+      vCrunch.balanceOf(beneficiary)
+    ).to.eventually.be.a.bignumber.equal(ZERO);
+
+    await expect(
+      multiVesting.create(beneficiary, amount, time.days(1), time.days(10))
+    ).to.be.fulfilled;
+
+    await expect(
+      vCrunch.balanceOf(beneficiary)
+    ).to.eventually.be.a.bignumber.equal(amount);
+
+    await advance.timeAndBlock(time.days(5));
+
+    await expect(multiVesting.releaseAllFor(beneficiary)).to.be.fulfilled;
+
+    await expect(
+      vCrunch.balanceOf(beneficiary)
+    ).to.eventually.be.a.bignumber.equal(amount.divn(2));
+
+    await advance.timeAndBlock(time.days(5));
+
+    await expect(multiVesting.releaseAllFor(beneficiary)).to.be.fulfilled;
+
+    await expect(
+      vCrunch.balanceOf(beneficiary)
+    ).to.eventually.be.a.bignumber.equal(ZERO);
+  });
+});
