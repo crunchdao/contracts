1355fbab12252db1329e68a7941486866d599f47
Caceresenzo <caceresenzo1502@gmail.com>
2022-03-05 17:57:46 +0100

    test(contract/vesting2): `setUnrevokable()` and `balanceOf(address)`

--- test/CrunchVestingV2.test.js
+++ test/CrunchVestingV2.test.js
@@ -12,39 +12,85 @@ contract("Crunch Vesting", async (accounts) => {
   let vesting;
 
   const [owner, beneficiary] = accounts;
+  const fromOwner = {
+    from: owner,
+  };
+  const fromBeneficiary = {
+    from: beneficiary,
+  };
 
-  const amount = new BN(1000);
-  const cliff = new BN(time.days(2));
-  const duration = new BN(time.days(10));
+  const defaults = {
+    cliff: new BN(time.days(2)),
+    duration: new BN(time.days(10)),
+  };
 
-  beforeEach(async () => {
-    crunch = await CrunchToken.new();
-
-    vesting = await CrunchVestingV2.new(
+  const createVesting = async ({
+    cliff = defaults.cliff,
+    duration = defaults.duration,
+    revokable = false,
+  } = {}) => {
+    return await CrunchVestingV2.new(
       crunch.address,
       beneficiary,
       cliff,
       duration,
-	  false
+      revokable
     );
+  };
 
-    await crunch.transfer(vesting.address, amount);
+  beforeEach(async () => {
+    crunch = await CrunchToken.new();
   });
 
   it("owner()", async () => {
+    vesting = await createVesting();
+
     await expect(vesting.owner()).to.eventually.be.equal(owner);
   });
 
   it("crunch()", async () => {
+    vesting = await createVesting();
+
     await expect(vesting.crunch()).to.eventually.be.equal(crunch.address);
   });
 
+  it("name()", async () => {
+    vesting = await createVesting();
+
+    await expect(vesting.name()).to.eventually.be.equal(
+      "Vested CRUNCH Token (single)"
+    );
+  });
+
+  it("symbol()", async () => {
+    vesting = await createVesting();
+
+    await expect(vesting.symbol()).to.eventually.be.equal("svCRUNCH");
+  });
+
+  it("decimals()", async () => {
+    vesting = await createVesting();
+
+    await expect(vesting.decimals()).to.eventually.be.a.bignumber.equal(
+      await crunch.decimals()
+    );
+  });
+
   it("remainingAmount()", async () => {
+    vesting = await createVesting();
+
     await expect(vesting.remainingAmount()).to.eventually.be.a.bignumber.equal(
-      amount
+      ZERO
     );
 
-    await advance.timeAndBlock(duration.divn(2));
+    const amount = new BN(100);
+    await expect(crunch.transfer(vesting.address, amount)).to.be.fulfilled;
+
+    await expect(vesting.remainingAmount()).to.eventually.be.a.bignumber.equal(
+      new BN(amount)
+    );
+
+    await advance.timeAndBlock(defaults.duration.divn(2));
 
     await expect(vesting.release()).to.be.fulfilled;
 
@@ -52,7 +98,7 @@ contract("Crunch Vesting", async (accounts) => {
       amount.divn(2)
     );
 
-    await advance.timeAndBlock(duration.divn(2));
+    await advance.timeAndBlock(defaults.duration.divn(2));
 
     await expect(vesting.release()).to.be.fulfilled;
 
@@ -61,45 +107,153 @@ contract("Crunch Vesting", async (accounts) => {
     );
   });
 
+  it("balanceOf(address) : beneficiary", async () => {
+    vesting = await createVesting();
+
+    await expect(
+      vesting.balanceOf(beneficiary)
+    ).to.eventually.be.a.bignumber.equal(ZERO);
+
+    const amount = new BN(100);
+    await expect(crunch.transfer(vesting.address, amount)).to.be.fulfilled;
+
+    await expect(
+      vesting.balanceOf(beneficiary)
+    ).to.eventually.be.a.bignumber.equal(amount);
+
+    await advance.timeAndBlock(defaults.duration);
+
+    await expect(vesting.release()).to.be.fulfilled;
+
+    await expect(
+      vesting.balanceOf(beneficiary)
+    ).to.eventually.be.a.bignumber.equal(ZERO);
+  });
+
+  it("balanceOf(address) : owner", async () => {
+    vesting = await createVesting();
+
+    await expect(vesting.balanceOf(owner)).to.eventually.be.a.bignumber.equal(
+      ZERO
+    );
+
+    const amount = new BN(100);
+    await expect(crunch.transfer(vesting.address, amount)).to.be.fulfilled;
+
+    await expect(vesting.balanceOf(owner)).to.eventually.be.a.bignumber.equal(
+      ZERO
+    );
+
+    await advance.timeAndBlock(defaults.duration);
+
+    await expect(vesting.release()).to.be.fulfilled;
+
+    await expect(vesting.balanceOf(owner)).to.eventually.be.a.bignumber.equal(
+      ZERO
+    );
+  });
+
+  it("setUnrevokable() : revokable", async () => {
+    vesting = await createVesting({ revokable: true });
+
+    await expect(vesting.setUnrevokable(fromBeneficiary)).to.be.rejectedWith(
+      Error,
+      "Ownable: caller is not the owner"
+    );
+
+    await expect(vesting.setUnrevokable()).to.be.fulfilled;
+    await expect(vesting.revokable()).to.eventually.be.equal(false);
+
+    await expect(vesting.setUnrevokable()).to.be.rejectedWith(
+      Error,
+      "Vesting: must be revokable"
+    );
+  });
+
+  it("setUnrevokable() : not revokable", async () => {
+    vesting = await createVesting({ revokable: false });
+
+    await expect(vesting.setUnrevokable(fromBeneficiary)).to.be.rejectedWith(
+      Error,
+      "Ownable: caller is not the owner"
+    );
+
+    await expect(vesting.setUnrevokable()).to.be.rejectedWith(
+      Error,
+      "Vesting: must be revokable"
+    );
+  });
+
+  it("setUnrevokable() : revoked", async () => {
+      try {
+        vesting = await createVesting({ revokable: true });
+    
+        await expect(vesting.revoke()).to.be.fulfilled;
+    
+        await expect(vesting.setUnrevokable(fromBeneficiary)).to.be.rejectedWith(
+          Error,
+          "Ownable: caller is not the owner"
+        );
+    
+        await expect(vesting.setUnrevokable()).to.be.rejectedWith(
+          Error,
+          "Vesting: already revoked"
+        );
+      } catch (error) {
+          console.log(error)
+      }
+  });
+
   it("setCrunch(address)", async () => {
+    vesting = await createVesting();
+
     const nulled = "0x0000000000000000000000000000000000000000";
     const dummy = "0x4242424242424242424242424242424242424242";
-    const [, , account] = accounts;
 
-    /* not from owner */
-    await expect(vesting.setCrunch(dummy, { from: account })).to.be.rejected;
+    await expect(vesting.setCrunch(dummy, fromBeneficiary)).to.be.rejectedWith(
+      Error,
+      "Ownable: caller is not the owner"
+    );
 
-    /* null address */
-    await expect(vesting.setCrunch(nulled)).to.be.rejected;
+    await expect(vesting.setCrunch(nulled)).to.be.rejectedWith(
+      Error,
+      "Vesting: new crunch cannot be null"
+    );
+
+    await expect(vesting.setCrunch(crunch.address)).to.be.rejectedWith(
+      Error,
+      "Vesting: token address cannot be updated to the same value"
+    );
 
     await expect(vesting.setCrunch(dummy)).to.be.fulfilled;
     await expect(vesting.crunch()).to.eventually.be.equal(dummy);
   });
 
   it("transferBeneficiary(address)", async () => {
+    vesting = await createVesting();
+
     const nulled = "0x0000000000000000000000000000000000000000";
     const [, , account] = accounts;
 
-    /* not from owner */
-    await expect(vesting.transferBeneficiary(account, { from: account })).to.be
-      .rejected;
+    await expect(
+      vesting.transferBeneficiary(account, { from: account })
+    ).to.be.rejectedWith(Error, "Vesting: caller is not the beneficiary");
 
-    /* not from owner */
-    await expect(vesting.transferBeneficiary(account, { from: owner })).to.be
-      .rejected;
+    await expect(
+      vesting.transferBeneficiary(account, fromOwner)
+    ).to.be.rejectedWith(Error, "Vesting: caller is not the beneficiary");
 
-    /* null address */
-    await expect(vesting.transferBeneficiary(nulled, { from: beneficiary })).to
-      .be.rejected;
+    await expect(
+      vesting.transferBeneficiary(nulled, fromBeneficiary)
+    ).to.be.rejectedWith(Error, "Vesting: beneficiary cannot be null");
 
-    await expect(vesting.transferBeneficiary(account, { from: beneficiary })).to
-      .be.fulfilled;
+    await expect(vesting.transferBeneficiary(account, fromBeneficiary)).to.be
+      .fulfilled;
     await expect(vesting.beneficiary()).to.eventually.be.equal(account);
 
-    /* not from new owner */
     await expect(
-      vesting.transferBeneficiary(beneficiary, { from: beneficiary })
-    ).to.be.rejected;
+      vesting.transferBeneficiary(beneficiary, fromBeneficiary)
+    ).to.be.rejectedWith(Error, "Vesting: caller is not the beneficiary");
 
     await expect(vesting.transferBeneficiary(beneficiary, { from: account })).to
       .be.fulfilled;
