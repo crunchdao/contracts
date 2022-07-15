59dfc63bb9316dd09153e4b6c4ed4184746dd57f
Caceresenzo <caceresenzo1502@gmail.com>
2022-07-15 20:33:02 +0200

    test(contract/multi-vesting-2): testing revoke

--- test/CrunchMultiVestingV2.test.js
+++ test/CrunchMultiVestingV2.test.js
@@ -8,6 +8,11 @@ const CrunchMultiVestingV2 = artifacts.require("CrunchMultiVestingV2");
 
 const NULL = "0x0000000000000000000000000000000000000000";
 const ZERO = new BN("0");
+const ONE = new BN("1");
+const ONE_YEAR = new BN(timeHelper.years(1));
+const TWO_YEAR = new BN(timeHelper.years(2));
+
+const ONE_CRUNCH = new BN("1000000000000000000");
 
 contract("Crunch Multi Vesting V2", async ([owner, user, ...accounts]) => {
   const fromUser = { from: user };
@@ -66,7 +71,13 @@ contract("Crunch Multi Vesting V2", async ([owner, user, ...accounts]) => {
     ).to.eventually.be.a.bignumber.equal(amount);
 
     await expect(
-      multiVesting.create(owner, half, timeHelper.years(1), timeHelper.years(1))
+      multiVesting.create(
+        owner,
+        half,
+        timeHelper.years(1),
+        timeHelper.years(1),
+        true
+      )
     ).to.be.fulfilled;
 
     await expect(
@@ -101,10 +112,76 @@ contract("Crunch Multi Vesting V2", async ([owner, user, ...accounts]) => {
 
   it("create(address, uint256, uint256, uint256) : beneficiary=0x0", async () => {
     await expect(
-      multiVesting.create(NULL, ZERO, ZERO, ZERO)
+      multiVesting.create(NULL, ZERO, ZERO, ZERO, true)
     ).to.be.rejectedWith(
       Error,
       "MultiVesting: beneficiary is the zero address"
     );
+  });
+
+  it("revoke(address, index) : not revocable", async () => {
+    await expect(crunch.transfer(multiVesting.address, ONE)).to.be.fulfilled;
+
+    await expect(multiVesting.create(user, ONE, ONE, ONE, false)).to.be
+      .fulfilled;
+
+    await expect(multiVesting.revoke(user, ZERO)).to.be.rejectedWith(
+      Error,
+      "MultiVesting: token not revocable"
+    );
+  });
+
+  it("revoke(address, index) : already revoked", async () => {
+    await expect(crunch.transfer(multiVesting.address, ONE)).to.be.fulfilled;
+
+    await expect(multiVesting.create(user, ONE, ONE, ONE, true)).to.be
+      .fulfilled;
+
+    await expect(multiVesting.revoke(user, ZERO)).to.be.fulfilled;
+
+    await expect(multiVesting.revoke(user, ZERO)).to.be.rejectedWith(
+      Error,
+      "MultiVesting: token already revoked"
+    );
+  });
+
+  it("revoke(address, index) : before cliff", async () => {
+    await expect(crunch.transfer(multiVesting.address, ONE)).to.be.fulfilled;
+
+    await expect(multiVesting.create(user, ONE, ONE_YEAR, TWO_YEAR, true)).to.be
+      .fulfilled;
+
+    await expect(multiVesting.begin()).to.be.fulfilled;
+
+    await expect(multiVesting.revoke(user, ZERO)).to.be.fulfilled;
+
+    await advance.timeAndBlock(TWO_YEAR);
+
+    await expect(multiVesting.release(ZERO, fromUser)).to.be.rejectedWith(
+      Error,
+      "MultiVesting: no tokens are due"
+    );
+  });
+
+  it("revoke(address, index)", async () => {
+    await expect(crunch.transfer(multiVesting.address, ONE_CRUNCH)).to.be
+      .fulfilled;
+
+    await expect(
+      multiVesting.create(user, ONE_CRUNCH, ONE_YEAR, TWO_YEAR, true)
+    ).to.be.fulfilled;
+
+    await expect(multiVesting.begin()).to.be.fulfilled;
+
+    await advance.timeAndBlock(ONE_YEAR.add(ONE_YEAR.divn(2)));
+
+    await expect(multiVesting.revoke(user, ZERO)).to.be.fulfilled;
+
+    await advance.timeAndBlock(ONE_YEAR);
+
+    await expect(multiVesting.release(ZERO, fromUser)).to.be.fulfilled;
+
+    // TODO Check amount
+    await expect(crunch.balanceOf(user)).to.eventually.be.a.bignumber.not.equal(ZERO);
   });
 });
