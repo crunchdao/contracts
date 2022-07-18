d5f625f3a5c6e21c4f19cfea5f28aa38c953420e
Caceresenzo <caceresenzo1502@gmail.com>
2022-07-18 18:18:53 +0200

    test(contract/multi-vesting-2): added missing `create(address, uint256, uint256, uint256)` cases

--- test/CrunchMultiVestingV2.test.js
+++ test/CrunchMultiVestingV2.test.js
@@ -10,10 +10,21 @@ const NULL = "0x0000000000000000000000000000000000000000";
 const ZERO = new BN("0");
 const ONE = new BN("1");
 const TWO = new BN("2");
+const THREE = new BN("3");
 const ONE_YEAR = new BN(timeHelper.years(1));
 const TWO_YEAR = new BN(timeHelper.years(2));
 
 const ONE_CRUNCH = new BN("1000000000000000000");
+
+function expectVesting(got, expected) {
+  expect(got.beneficiary).to.be.equal(expected.beneficiary);
+  expect(got.amount).to.be.a.bignumber.equal(expected.amount);
+  expect(got.cliffDuration).to.be.a.bignumber.equal(expected.cliffDuration);
+  expect(got.duration).to.be.a.bignumber.equal(expected.duration);
+  expect(got.revocable).to.be.equal(expected.revocable);
+  expect(got.revoked).to.be.equal(expected.revoked);
+  expect(got.released).to.be.a.bignumber.equal(expected.released);
+}
 
 contract("Crunch Multi Vesting V2", async ([owner, user, ...accounts]) => {
   const fromUser = { from: user };
@@ -92,6 +103,46 @@ contract("Crunch Multi Vesting V2", async ([owner, user, ...accounts]) => {
     await expect(multiVesting.create(owner, ONE, ONE, ONE, true)).to.be.rejectedWith(Error, "MultiVesting: beneficiary is already vested");
   });
 
+  it("create(address, uint256, uint256, uint256) : amount=0", async () => {
+    await expect(multiVesting.create(owner, ZERO, ONE, ONE, true)).to.be.rejectedWith(Error, "MultiVesting: amount is 0");
+  });
+
+  it("create(address, uint256, uint256, uint256) : duration=0", async () => {
+    await expect(multiVesting.create(owner, ONE, ONE, ZERO, true)).to.be.rejectedWith(Error, "MultiVesting: duration is 0");
+  });
+
+  it("create(address, uint256, uint256, uint256) : cliff longer than duration", async () => {
+    await expect(multiVesting.create(owner, ONE, TWO, ONE, true)).to.be.rejectedWith(Error, "MultiVesting: cliff is longer than duration");
+  });
+
+  it("create(address, uint256, uint256, uint256) : no reserve", async () => {
+    await expect(multiVesting.create(owner, TWO, ONE, ONE, true)).to.be.rejectedWith(Error, "MultiVesting: available reserve is not enough");
+
+    await expect(crunch.transfer(multiVesting.address, ONE)).to.be.fulfilled;
+
+    await expect(multiVesting.create(owner, TWO, ONE, ONE, true)).to.be.rejectedWith(Error, "MultiVesting: available reserve is not enough");
+
+    await expect(multiVesting.create(owner, ONE, ONE, ONE, true)).to.be.fulfilled;
+  });
+
+  it("create(address, uint256, uint256, uint256)", async () => {
+    await expect(crunch.transfer(multiVesting.address, ONE)).to.be.fulfilled;
+    await expect(multiVesting.create(user, ONE, TWO, THREE, true)).to.be.fulfilled;
+
+    await expect(multiVesting.isVested(user)).to.be.eventually.true;
+
+    const vesting = await multiVesting.vestings(user);
+    expectVesting(vesting, {
+      beneficiary: user,
+      amount: ONE,
+      cliffDuration: TWO,
+      duration: THREE,
+      revocable: true,
+      revoked: false,
+      released: ZERO,
+    });
+  });
+
   it("transfer(address) : not vesting", async () => {
     await expect(multiVesting.transfer(user)).to.be.rejectedWith(Error, "MultiVesting: not currently vesting");
   });
@@ -166,7 +217,7 @@ contract("Crunch Multi Vesting V2", async ([owner, user, ...accounts]) => {
     // TODO Check amount
     await expect(crunch.balanceOf(user)).to.eventually.be.a.bignumber.not.equal(ZERO);
   });
-  
+
   it("clearFor(address) : not the owner", async () => {
     await expect(multiVesting.clearFor(user, fromUser)).to.be.rejectedWith(Error, "Ownable: caller is not the owner");
   });
